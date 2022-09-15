99de462119c4ef8227c9b2fdb2ad92840844bc2b
sujan pokharel <pokharelisujna@gmail.com>
2022-09-16 00:48:39 +0900

    first commit

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/ssb/")
+@RequestMapping("")
 public class HomeController {
   
   @GetMapping("/home")
