4686190a00d2cf7c0852b2037c48724b5fb7a16d
Homer <subarnabohara@gmail.com>
2022-09-17 20:39:55 +0900

    jj

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
   @GetMapping("/homer")
-  public String creatHome() {
+  public String   creatHome() {
     return "home";
   }
 
