f41d922ffc6c43201f771d61c16fd318617dea1d
aryalbishnu <ariyalbishnu@gmail.com>
2022-09-18 22:55:49 +0900

    add something file

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,9 +9,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("")
 public class HomeController {
   
-  @GetMapping("/home")
-  public String creatHomer() {
+  @GetMapping("/")
+  public String creatHomer(Model model) {
+	  model.addAttribute("title", "SSB_home_page");
     return "home";
   }
+  
+  @GetMapping("/abouts")
+  public String abouts(Model model) {
+	  model.addAttribute("title", "SSB_about_page");
+	  return "abouts";
+	  
+  }
+  
+  @GetMapping("/services")
+  public String services(Model model) {
+	  model.addAttribute("title", "SSB_sercices_page");
+	  return "services";
+  }
+  
+
+  @GetMapping("/gmail")
+  public String sengemail(Model model) {
+	  model.addAttribute("title", "SSB_client_support_email_page");
+	  return "gmail";
+  }
+	  
+
 
 }
---
+++ src/main/resources/static/js/timeScript.js
@@ -0,0 +1,32 @@
+const options= {weekday: 'long', year: 'numeric', month: 'long', day:'numeric'}
+setInterval(() => {
+    let time= new Date();
+let hour= time.getHours();
+let timeofDay = (hour<12)? "AM" : "PM";
+if(hour>12){
+    hour= hour-12;
+}
+if(hour<10){
+    hour= "0"+hour
+}
+if(hour===0){
+    hour=12;
+}
+let minute= time.getMinutes();
+if(minute<10){
+    minute= "0" + minute;
+}
+let second= time.getSeconds();
+if(second<10){
+    second= "0" + second
+}
+
+let date= time.toLocaleDateString(undefined, options);
+let setTime= hour +":"+ minute + ":"+ second + "  " + timeofDay ;
+document.getElementById("timeSet").innerHTML=setTime + "  " + date;
+
+
+
+}, 1000);
+
+
